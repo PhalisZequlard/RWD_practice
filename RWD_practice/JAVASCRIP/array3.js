bc2521c99fdfe31151b88bf5482389cba65db50f
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-02-28 11:37:45 +0800

    20230228

--- RWD_practice/JAVASCRIP/array3.js
+++ RWD_practice/JAVASCRIP/array3.js
@@ -46,15 +46,38 @@ let drinks = [
 
 // foreach
 users.forEach(function(data){
-    console.log(data.mail);
+    // console.log(data.mail);
 })
 
 // map
-let drinkName = drinks.map(function(data){
+let drinkName = users.map(function(data){
     return data.name;
 })
 let drinkPrice = drinks.map(function(data){
     return data.price;
 })
-console.log(drinkName);
+// console.log(drinkName);
+// console.log(drinkPrice);
+
+// filter
+let cheap = drinks.filter(function(data){
+    return data.price <= 30;
+})
+let bubble = drinks.filter(function(data){
+    return data.name == '珍珠奶茶';
+})
+drinks = drinks.filter(function(data){
+    return data.name != '綠茶';
+})
+console.log(drinks);
+
+// includes
+let result = drinkName.includes('摩卡');
+console.log(result);
+
+// reduce 
 console.log(drinkPrice);
+let total = drinkPrice.reduce(function(a,b){
+    return a + b;
+})
+console.log(total);
